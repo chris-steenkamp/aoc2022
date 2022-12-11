ad40c2d27b487a743a83fe5094a83a2473e88b52
Chris Steenkamp <cdsteenkamp@gmail.com>
2022-12-11 08:41:45 +0200

    Move each day into its own folder

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
 from unittest import TestCase
 
-from day10 import get_cycle_count, process_instructions, load_data
+from day10.day10 import get_cycle_count, process_instructions, load_data
 
 
 class Day10(TestCase):
     def setUp(self) -> None:
         self.samples = [
-            load_data("day10-sample1.txt"),
-            load_data("day10-sample2.txt"),
-            load_data("day10-input.txt"),
+            load_data("day10/day10-sample1.txt"),
+            load_data("day10/day10-sample2.txt"),
+            load_data("day10/day10-input.txt"),
         ]
 
-        with open("day10-sample-output.txt", "r") as f:
+        with open("day10/day10-sample-output.txt", "r") as f:
             self.sample_output = []
             for l in f:
                 self.sample_output.extend(char for char in l.strip())
